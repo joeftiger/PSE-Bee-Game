944e279f890a584199e639b8e3bd212caf83d9b9
j.oeftiger <j.oeftiger@protonmail.com>
2019-03-13 17:20:53 +0100

    Remove redundant method

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -7,13 +7,6 @@ Scene* GameScene::createScene()
     return GameScene::create();
 }
 
-// Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename)
-{
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in GameScene.cpp\n");
-}
-
 bool GameScene::init()
 {
     if ( !Scene::init()) return false;
