e56f2f97728ac23827bb80f040bcf490b9bf4e4a
tobiasbrugger <43401568+tobiasbrugger@users.noreply.github.com>
2019-03-26 14:21:33 +0100

    added comment to TileGID for clarity

--- Classes/HeaderFiles/TileGID.h
+++ Classes/HeaderFiles/TileGID.h
@@ -1,9 +1,18 @@
 #ifndef TILEGID_H
 #define TILEGID_H
 
+/**
+	flower = 1, 
+	bush1 = 2,
+	bush2 = 3, 
+	bush3 = 4,
+	bush4 = 5,
+	grass = 6,
+	road = 7
+*/
 enum TileGID {
-	flower = 1, //1
-	bush1, //2 usw.
+	flower = 1,
+	bush1,
 	bush2, 
 	bush3,
 	bush4,
