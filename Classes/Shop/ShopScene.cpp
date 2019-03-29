bdccd3a75ec6a20d627dee322198d987cf11f705
Aljaschue <aljaschue@gmail.com>
2019-03-29 15:16:23 +0100

    Added ShopScene

---
+++ Classes/Shop/ShopScene.cpp
@@ -0,0 +1,13 @@
+
+#include "ShopScene.h"
+
+using namespace cocos2d;
+
+Scene* ShopScene::createScene()
+{
+    return ShopScene::create();
+}
+
+bool ShopScene::init(){
+    return true;
+}
---
+++ Classes/Shop/ShopScene.h
@@ -0,0 +1,21 @@
+
+#ifndef PSE_BEE_GAME_SHOPSCENE_H
+#define PSE_BEE_GAME_SHOPSCENE_H
+
+#include "cocos2d.h"
+#include "HeaderFiles/DEFINITIONS.h"
+
+
+class ShopScene : public cocos2d::Scene {
+private:
+public:
+    static cocos2d::Scene* createScene();
+    virtual bool init();
+
+
+    CREATE_FUNC(ShopScene);
+
+};
+
+
+#endif //PSE_BEE_GAME_SHOPSCENE_H
