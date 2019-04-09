079e1fa6b4ca6a745ad33385f67ed0a04f3292f6
Julius Oeftiger <j.oeftiger@protonmail.com>
2019-04-09 11:34:09 +0200

    Remove FIXME

--- Classes/Game/BeeHiveAtlas.cpp
+++ Classes/Game/BeeHiveAtlas.cpp
@@ -11,8 +11,6 @@ BeeHiveAtlas *BeeHiveAtlas::_instance = nullptr;
 BeeHiveAtlas *BeeHiveAtlas::getInstance() {
 	if (!_instance) {
 		_instance = new BeeHiveAtlas;
-		// FIXME: The scheduler apparently does not run, as BeeHiveAtlas::update is never called!
-		//_instance->schedule(schedule_selector(BeeHiveAtlas::update), 1.0f);
 		Director::getInstance()->getScheduler()->schedule(schedule_selector(BeeHiveAtlas::update), _instance, 1.0f, false);
 	}
 	
@@ -64,7 +62,7 @@ void BeeHiveAtlas::notify(void *observable) {
 }
 
 void BeeHiveAtlas::update(float dt) {
-	//cocos2d::log("%f", dt);
+	//cocos2d::log("BeeHiveAtlas:\tUpdating beehives.");
 	for (auto bh : _beeHives) {
 		bh->update();
 	}
