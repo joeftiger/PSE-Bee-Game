2e1cd03aa35b7622719108b34c57c488fd912f96
Julius Oeftiger <j.oeftiger@protonmail.com>
2019-04-14 17:25:09 +0200

    Placeable: Add small class comment

--- Classes/TileMapObjects/Placeable.h
+++ Classes/TileMapObjects/Placeable.h
@@ -7,16 +7,21 @@
 
 #include "cocos2d.h"
 
+/**
+ * A placeable object is handled by the TileMapLayer to place tile/trees/houses etc.
+ */
 class Placeable {
 public:
     /**
      * Returns the size of this placeable object (in tiles).
+     * Needed to check for obstructions
      * @return size in tiles
      */
     virtual cocos2d::Size size() = 0;
 
     /**
      * Returns the center of this placeable object (in tiles).
+     * Needed to precisely place the
      * @return center position
      */
     virtual cocos2d::Vec2 center() = 0;
