a34df3418120612fdc3cee5bd27ed25c39a9fd00
Julius Oeftiger <j.oeftiger@protonmail.com>
2019-04-15 16:13:19 +0200

    BeehiveAtlas: getBeeHiveAt(Vec2 position)

--- Classes/Game/BeeHiveAtlas.h
+++ Classes/Game/BeeHiveAtlas.h
@@ -31,6 +31,13 @@ public:
 	void getBeeHives(std::vector <BeeHive *> &beeHives);
 
 	/**
+	 * Returns the BeeHive object at given position.
+	 * @param pos position of beehive in tilemap
+	 * @return beehive object
+	 */
+	BeeHive *getBeeHiveAt(const Vec2& pos);
+
+	/**
 	 * Notifies this BeeHiveAtlas, that the observable has been changed.
 	 * The observable should be a TileMapLayer.
 	 * @param observable
