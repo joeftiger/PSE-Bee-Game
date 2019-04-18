2077bb0585c5f47b968c4b8165fa5c3054ae496a
Brian6330 <brian@schweigler.org>
2019-04-18 15:43:33 +0200

    documentation added and slight refactor

--- Classes/About/AboutScene.cpp
+++ Classes/About/AboutScene.cpp
@@ -15,6 +15,7 @@ Scene *AboutScene::createScene() { return AboutScene::create(); }
 bool AboutScene::init() {
 	if (!Scene::init()) return false;
 
+	// get size related information regarding the screen
 	cocos2d::Rect visibleRect = Director::getInstance()->getOpenGLView()->getVisibleRect();
 	auto visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
@@ -22,33 +23,27 @@ bool AboutScene::init() {
 	onePofScreenH = visibleSize.height / 100;
 	onePofScreenW = visibleSize.width / 100;
 
-	// Create a title to identify
+	// create a title and set it on the top of the screen
 	auto title = Label::createWithTTF("About", "fonts/OpenSans-ExtraBold.ttf", 48);
-
-	// position the label on the top center of the screen
 	title->setPosition(Vec2(origin.x + visibleSize.width / 2,
 	                        origin.y + visibleSize.height - title->getContentSize().height));
-
-	// add the label as a child to this layer
 	this->addChild(title, 1);
 
+	// create a richtext button with text about many relevant things
+	// TODO Add information, links and maybe images? (18.4)
 	auto text = cocos2d::ui::RichText::create();
 	const auto font = std::string("fonts/OpenSans-Regular.ttf");
 	text->ignoreContentAdaptWithSize(false);
 	text->setContentSize(Size(visibleSize.width - 40, visibleSize.height - 40));
-
 	text->pushBackElement(ui::RichElementText::create(0, Color3B::WHITE, 0xff,
 	                                                  "Created in cooperation with by Abilium by university of Berne students as part of the Praktikum Software Engineering lecture.",
 	                                                  font, 24));
-
 	text->setPosition(Vec2(origin.x + visibleSize.width / 2,
 	                       origin.y + visibleSize.height - title->getContentSize().height - 80));
-
 	this->addChild(text, 2);
 
 
-
-	//add the menu item for back to main menu
+	// add the button for back to main menu on the bottom right corner of the screen
 	auto label = Label::createWithTTF("Main Menu", "fonts/ReemKufi-Regular.ttf", 20);
 	auto menuItem = MenuItemLabel::create(label);
 	menuItem->setCallback([&](cocos2d::Ref *sender) {
