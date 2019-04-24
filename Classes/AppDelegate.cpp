faae8a94cc80b81c02ee06d674643708a0b73583
Aljaschue <aljaschue@gmail.com>
2019-04-24 16:23:57 +0200

    trying out Story Popups

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -25,6 +25,7 @@
 #include "AppDelegate.h"
 #include "Game/GameScene.h"
 #include "MainMenu/MainMenuScene.h"
+#include "Story/StoryScene.h"
 
 USING_NS_CC;
 
@@ -96,7 +97,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
 
 	// create a scene. it's an autorelease object
 	//auto scene = GameScene::createScene();
-	auto scene = MainMenu::scene();
+	auto scene = StoryScene::createScene();
 
 	// run
 	director->runWithScene(scene);
--- Classes/Story/StoryScene.cpp
+++ Classes/Story/StoryScene.cpp
@@ -1,6 +1,7 @@
 
 
 #include "StoryScene.h"
+#include "Popup.h"
 
 
 Scene *StoryScene::createScene() { 	return StoryScene::create(); }
@@ -9,8 +10,11 @@ bool StoryScene::init() {
 
 	if (!Scene::init()) {
 		return false;
+
 	}
 
+    UICustom::Popup *popup = UICustom::Popup::createAsMessage("Test 2", "This is a Message Popup");
+    this->addChild(popup);
 
 	return true;
 }
