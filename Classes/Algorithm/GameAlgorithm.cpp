71cbff8231c2f6ac49ff365fb191d756ffdef702
urogna <q.mauro@gmail.com>
2019-04-27 11:32:13 +0200

    merda

---
+++ Classes/Algorithm/GameAlgorithm.cpp
@@ -0,0 +1,13 @@
+//
+// Created by uro on 4/25/19.
+//
+
+#include "GameAlgorithm.h"
+#include "Game/BeeHive.h"
+
+
+//float honey = 450000/(6*30*24*60*60*50000);
+
+//float GameAlgorithm::honeyPerBee() {
+    //BeeHiveAtlas::getInstance()->getBeeHives(_beeHives);
+//}
---
+++ Classes/Algorithm/GameAlgorithm.h
@@ -0,0 +1,24 @@
+//
+// Created by uro on 4/25/19.
+//
+
+#ifndef PSE_BEE_GAME_GAMEALGORITHM_H
+#define PSE_BEE_GAME_GAMEALGORITHM_H
+
+#include "Game/BeeHive.h"
+#include "Atlas/BeeHiveAtlas.h"
+
+/**
+ * class GameAlgorithm {
+private:
+    std::vector<BeeHive&> _beeHives;
+
+public:
+    float honeyPerBee();
+
+};
+
+ */
+
+
+#endif //PSE_BEE_GAME_GAMEALGORITHM_H
