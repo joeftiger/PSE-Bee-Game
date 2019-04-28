91d9b018859f5624402534dbaa6a78fd40b2ada4
Julius Oeftiger <j.oeftiger@protonmail.com>
2019-04-28 21:38:58 +0200

    SpriteContainer: Contains sprites and tiles

---
+++ Classes/Resources/SpriteContainer.cpp
@@ -0,0 +1,62 @@
+//
+// Created by julius on 28.04.19.
+//
+
+#include "SpriteContainer.h"
+
+SpriteContainer::SpriteContainer() {
+    std::string path = "tilemaps/Tiles/";
+
+    _GIDsprites.emplace(Tiles::beehiveBig1    , Sprite::create(path + "bienenstock1_gross.png"));
+    _GIDsprites.emplace(Tiles::beehiveSmall1  , Sprite::create(path + "bienenstock1_klein.png"));
+    _GIDsprites.emplace(Tiles::beehiveMiddle1 , Sprite::create(path + "bienenstock1_mittel.png"));
+    _GIDsprites.emplace(Tiles::beehiveBig2    , Sprite::create(path + "bienenstock2_gross.png"));
+    _GIDsprites.emplace(Tiles::beehiveSmall2  , Sprite::create(path + "bienenstock2_klein.png"));
+    _GIDsprites.emplace(Tiles::beehiveMiddle2 , Sprite::create(path + "bienenstock2_mittel.png"));
+    _GIDsprites.emplace(Tiles::beehiveBig3    , Sprite::create(path + "bienenstock3_gross.png"));
+    _GIDsprites.emplace(Tiles::beehiveMiddle3 , Sprite::create(path + "bienenstock3_mittel.png"));
+    _GIDsprites.emplace(Tiles::flower1        , Sprite::create(path + "blumen1_spring_summmer.png"));
+    _GIDsprites.emplace(Tiles::flower2        , Sprite::create(path + "blumen2_spring_summmer.png"));
+    _GIDsprites.emplace(Tiles::flower3        , Sprite::create(path + "blumen3_spring_summmer.png"));
+    _GIDsprites.emplace(Tiles::flower4        , Sprite::create(path + "blumen4_spring_summmer.png"));
+    _GIDsprites.emplace(Tiles::bush1          , Sprite::create(path + "busch1_spring_summmer.png"));
+    _GIDsprites.emplace(Tiles::bush2          , Sprite::create(path + "busch2_spring_summmer.png"));
+    _GIDsprites.emplace(Tiles::bush3          , Sprite::create(path + "busch3_spring_summmer.png"));
+    _GIDsprites.emplace(Tiles::bush4          , Sprite::create(path + "busch4_spring_summmer.png"));
+    _GIDsprites.emplace(Tiles::grass          , Sprite::create(path + "grasflàche.png"));
+    _GIDsprites.emplace(Tiles::road           , Sprite::create(path + "steinplattenboden.png"));
+    // these are currently only in the spritesheetSD.png
+    _GIDsprites.emplace(Tiles::obstruction    , nullptr);
+    _GIDsprites.emplace(Tiles::no_obstruction , nullptr);
+
+    path = "Tilemaps/Sprites";
+    _IDsprites.emplace(Sprites::tree_1        , Sprite::create(path + "baum1_spring_summer.png"));
+    _IDsprites.emplace(Sprites::tree_1_apple_1, Sprite::create(path + "baum1_apfel1.png"));
+    _IDsprites.emplace(Sprites::tree_1_apple_2, Sprite::create(path + "baum1_apfel2.png"));
+    _IDsprites.emplace(Sprites::tree_1_apple_3, Sprite::create(path + "baum1_apfel3.png"));
+    _IDsprites.emplace(Sprites::tree_2        , Sprite::create(path + "baum2_spring_summer.png"));
+    _IDsprites.emplace(Sprites::tree_2_apple_1, Sprite::create(path + "baum2_apfel1.png"));
+    _IDsprites.emplace(Sprites::tree_2_apple_2, Sprite::create(path + "baum2_apfel2.png"));
+    _IDsprites.emplace(Sprites::tree_2_apple_3, Sprite::create(path + "baum2_apfel3.png"));
+    _IDsprites.emplace(Sprites::tree_3_summer , Sprite::create(path + "baum3_spring_summer.png"));
+    _IDsprites.emplace(Sprites::tree_3_winter , Sprite::create(path + "baum3_winter.png"));
+    _IDsprites.emplace(Sprites::tree_4        , Sprite::create(path + "baum4_spring_summer.png"));
+    _IDsprites.emplace(Sprites::tree_4_apple_1, Sprite::create(path + "baum4_apfel1.png"));
+    _IDsprites.emplace(Sprites::tree_4_apple_2, Sprite::create(path + "baum4_apfel2.png"));
+    _IDsprites.emplace(Sprites::tree_4_apple_3, Sprite::create(path + "baum4_apfel3.png"));
+    _IDsprites.emplace(Sprites::erlenmeyer    , Sprite::create(path + "erlenmeyerkolben.png"));
+    _IDsprites.emplace(Sprites::honey_glass_2d, Sprite::create(path + "honigglas_2d.png"));
+    _IDsprites.emplace(Sprites::honey_glass_3d, Sprite::create(path + "honigglas_3d.png"));
+    _IDsprites.emplace(Sprites::honey_bucket  , Sprite::create(path + "honigkessel.png"));
+    _IDsprites.emplace(Sprites::honey_extractor,Sprite::create(path + "honigschleuder.png"));
+    _IDsprites.emplace(Sprites::coin_single   , Sprite::create(path + "münze_einzeln.png"));
+    _IDsprites.emplace(Sprites::coin_pile     , Sprite::create(path + "münze_haufen.png"));
+}
+
+Sprite *SpriteContainer::getSpriteOf(Tiles::TileGID &gid) {
+    return _GIDsprites[gid];
+}
+
+Sprite *SpriteContainer::getSpriteOf(Sprites::SpriteID &id) {
+    return _IDsprites[id];
+}
