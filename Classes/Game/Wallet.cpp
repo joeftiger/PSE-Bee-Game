87cf3e93af929ffaadf4acbd4cf588caf4d87b81
Brian6330 <brian@schweigler.org>
2019-04-29 13:35:26 +0200

    Wallet skeleton

---
+++ Classes/Game/Wallet.cpp
@@ -0,0 +1,12 @@
+//
+// Created by brian on 29/04/2019.
+//
+
+#include "Wallet.h"
+
+
+Wallet  *Wallet::_instance = nullptr;
+
+bool Wallet::invariant() {
+	return true;
+}
---
+++ Classes/Game/Wallet.h
@@ -0,0 +1,19 @@
+//
+// Created by brian on 29/04/2019.
+//
+
+#ifndef PROJ_ANDROID_WALLET_H
+#define PROJ_ANDROID_WALLET_H
+
+#endif //PROJ_ANDROID_WALLET_H
+
+
+class Wallet {
+private:
+	static Wallet *_instance;
+
+	bool invariant();
+
+public:
+
+};
