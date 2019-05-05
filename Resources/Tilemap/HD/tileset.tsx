ffd1232dfb990e02a9e600b9f345546da5e38729
Julius Oeftiger <j.oeftiger@protonmail.com>
2019-05-05 12:13:53 +0200

    Resources: New tilemap with new tileset

---
+++ Resources/Tilemap/HD/tileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tileset" tilewidth="194" tileheight="264" tilecount="70" columns="10">
+ <grid orientation="isometric" width="194" height="264"/>
+ <image source="tilesheet.png" width="2048" height="2048"/>
+</tileset>
