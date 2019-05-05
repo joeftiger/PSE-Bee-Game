6a284d548cd03d223d106e18791cba00fbc0434a
Julius Oeftiger <j.oeftiger@protonmail.com>
2019-05-05 12:14:22 +0200

    Resources: Removed unused (deprecated) folders

--- Resources/tilemaps/spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="spritesheet" tilewidth="777" tileheight="1054" tilecount="20" columns="5">
- <image source="spritesheet.png" width="3885" height="4216"/>
-</tileset>
