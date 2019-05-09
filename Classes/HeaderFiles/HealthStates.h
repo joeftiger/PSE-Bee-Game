9bf37f41a5b95e87e70051a55cff158186a3a09e
Brian6330 <brian@schweigler.org>
2019-05-09 15:24:06 +0200

    HealthStates and CmakeList init

---
+++ Classes/HeaderFiles/HealthStates.h
@@ -0,0 +1,15 @@
+
+#ifndef PROJ_ANDROID_HEALTHSTATES_H
+#define PROJ_ANDROID_HEALTHSTATES_H
+
+/**
+ * List of the 4 health states
+ */
+enum HealthStates {
+	Dead = 0.2f,
+	Bad,
+	Okay,
+	Good
+};
+
+#endif //PROJ_ANDROID_HEALTHSTATES_H
