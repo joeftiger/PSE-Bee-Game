596023549d77a28d7f402acbb5788f186f2d7d89
Julius Oeftiger <j.oeftiger@protonmail.com>
2019-05-09 12:12:32 +0200

    Sprites: getSeasonalSpriteIDof(id, season)

--- Classes/Resources/Sprites.h
+++ Classes/Resources/Sprites.h
@@ -7,6 +7,7 @@
 
 
 #include <cocos/2d/CCSprite.h>
+#include <HeaderFiles/Seasons.h>
 
 class Sprites {
 public:
@@ -50,11 +51,29 @@ public:
     };
 
     /**
+	 * Returns whether the given sprite id is of type honey extractor.
+	 * @param id sprite id
+	 * @return <code>true</code> if honey extractor. <code>false</code> if not.
+	 */
+    static bool isHoneyExtractor(SpriteID id);
+
+    /**
 	 * Returns the sprite corresponding to the given sprite id.
 	 * @param id id of sprite
 	 * @return sprite
 	 */
     static cocos2d::Sprite *getSpriteOf(SpriteID id);
+
+    /**
+	 * Returns the seasonal sprite id of the given sprite id according to the season.
+     * If not a tree, the sprite id itself will be returned.
+     * The seasonal sprites contain no apples.
+     *
+	 * @param id sprite gid
+	 * @param season season to look up
+	 * @return corresponding seasonal sprite id
+	 */
+    static Sprites::SpriteID getSeasonSpriteIDof(SpriteID id, Season season);
 };
 
 
