9a5c5e42ab8adfaa3640db5f7668fac7b262c0de
Julius Oeftiger <julius.oeftiger@protonmail.com>
2019-05-12 12:22:31 +0200

    Definitions: Fix map / tree sizes

--- Classes/HeaderFiles/DEFINITIONS.h
+++ Classes/HeaderFiles/DEFINITIONS.h
@@ -10,18 +10,16 @@
 
 #define TILE_MAP_LAYER_NAME "TileMapLayer"
 
-#define MAP_SCALE_SD 0.5f
-#define MAP_SCALE_HD 0.5f
+#define MAP_SCALE_HD 1.0f
+#define MAP_SCALE_SD (MAP_SCALE_HD * 2)
 
-#define TREE_SCALE_HD (2.0f / 3)
-#define TREE_SCALE_SD (MAP_SCALE_SD * TREE_SCALE_HD)
+#define TREE_SCALE_HD (MAP_SCALE_HD * 4.0f / 3)
+#define TREE_SCALE_SD (TREE_SCALE_HD)   // Apparently, if you switch to MAP_SCALE_SD, it also affects the TREE_SCALE in general
 
 #define UPDATE_TIME 0.01f
 
 #define TEXT_SIZE_HUD 20
 #define FONT "fonts/ReemKufi-Regular.ttf"
-
-#define USE_SD true
 
 /* Time */
 #define LENGTH_MONTH 1 //in minutes
