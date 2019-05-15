7858ad485f561b6da5c73c396c98e6cccbce8223
Brian6330 <brian@schweigler.org>
2019-05-15 21:01:21 +0200

    Lots of About scene changes

--- Classes/About/AboutScene.cpp
+++ Classes/About/AboutScene.cpp
@@ -17,8 +17,17 @@ bool AboutScene::init() {
 	auto visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+	auto *backgroundAbout = cocos2d::Sprite::create("menu/background.png");
+	backgroundAbout->setAnchorPoint(Vec2(0.0f, 0.5f));
+    backgroundAbout->setPosition(Vec2(0, origin.y));
+    this->addChild(backgroundAbout, -1000);
+
 	onePofScreenH = visibleSize.height / 100;
 	onePofScreenW = visibleSize.width / 100;
+
+    Vec2 buttonsBed = Vec2(backgroundAbout->getBoundingBox().size.width / 2 + 153.0f,
+                           backgroundAbout->getBoundingBox().size.height / 2 - 3.0f);
+
 
 	// create a title and set it on the top of the screen
 	auto title = Label::createWithTTF("About", "fonts/OpenSans-ExtraBold.ttf", 48);
@@ -28,27 +37,30 @@ bool AboutScene::init() {
 
 	// create a richtext button with text about many relevant things
 	auto text = cocos2d::ui::RichText::create();
-	const auto font = std::string("fonts/OpenSans-Regular.ttf");
+	const auto font = std::string("fonts/OpenSans-Bold.ttf");
 	text->ignoreContentAdaptWithSize(false);
-	text->setContentSize(Size(visibleSize.width - 40, visibleSize.height - 40));
+	text->setContentSize(Size(visibleSize.width - 40, visibleSize.height - 10));
 	text->pushBackElement(ui::RichElementText::create(0, Color3B::WHITE, 0xff,
-                    "Das Spiel wurde erstellt in Zusammenarbeit mit dem Unternehmen Abilium GmbH, von Studenten an der Universität Bern, \n"
-                    "als Teil der Vorlesung Praktikum Software Engineering, unter Beratung von Mario Hess.\n\n"
+                    "Das Spiel wurde erstellt in Zusammenarbeit mit dem Unternehmen Abilium GmbH, von Studenten an \n"
+                    "der Universität Bern, als Teil der Vorlesung Praktikum Software Engineering, unter Beratung von Mario Hess.\n\n"
                     "Wir sind:\n"
                     "Mauro Quarta\n"
                     "Alec Schuermann\n"
                     "Tobias Brugger\n"
-                    "Brian Schweigler"
-                    , font, 26));
+                    "Brian Schweigler\n\n"
+                    "Artworks von Olivier Fontaine, Logo von Mauro Quarto\n\n"
+                    "Mehr Informationen über Bienen:\n"
+                    "Film: 'More Than Honey' (2012) von Markus Imhoof"
+                    , font, 27));
 
-
-	text->setPosition(Vec2(origin.x + visibleSize.width / 2,
-	                       origin.y + visibleSize.height - title->getContentSize().height - 100));
-	this->addChild(text, 2);
+    text->setAnchorPoint(Vec2(0.61f, -0.5f));
+	text->setPosition(Vec2(buttonsBed.x, buttonsBed.y));
+	backgroundAbout->addChild(text);
 
 
 	// add the button for back to main menu on the bottom right corner of the screen
 	auto label = Label::createWithTTF("Main Menu", "fonts/ReemKufi-Regular.ttf", 20);
+	label->enableOutline(Color4B::BLACK, 1);
 	auto menuItem = MenuItemLabel::create(label);
 	menuItem->setCallback([&](cocos2d::Ref *sender) {
 		Director::getInstance()->replaceScene(MainMenu::scene());
@@ -56,7 +68,7 @@ bool AboutScene::init() {
 	auto backMenu = Menu::create(menuItem, nullptr);
 	backMenu->setPosition(Vec2::ZERO);
 	backMenu->setPosition(Vec2(visibleRect.origin.x + visibleRect.size.width - onePofScreenW * 8,
-	                           visibleRect.origin.y + onePofScreenH * 5));
+                                               visibleRect.origin.y + onePofScreenH * 6));
 	this->addChild(backMenu, 10);
 
 
